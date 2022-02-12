7faf7b2e8ab8671c800d9c7d46d08569f9aef6de
yana-michelle <60404658+yana-michelle@users.noreply.github.com>
2022-02-11 23:32:07 -0600

    switch try

--- app.js
+++ app.js
@@ -17,3 +17,30 @@ const other = "In terms of strength? Spider-man.";
 const mostFun = "Loki of course."
 
 console.log (`who's the strongest avenger? ${name}, but ${other}. who's the funniest? ${mostFun}`);
+
+let day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+            day = "Monday";
+            break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+
+}
+document.getElementById("demo").innerHTML = "Today is" + day;
